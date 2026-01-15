38f2d37784b114b578ceb3512919aa50c075c15d
dufontdd <dufont.dd@gmail.com>
2026-01-15 15:04:53 +0100

    add all task

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+s = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+s = s[39:67] + s[-22:-17] + s[:6]
+print(s)
