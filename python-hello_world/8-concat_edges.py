35971357be9ae24f9eff3efb6846518e3d01e63f
dufontdd <dufont.dd@gmail.com>
2026-01-16 12:51:30 +0100

    print a string using different location of character of one string

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-s = "Python is an interpreted, interactive, object-oriented programming\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-s = s[39:67] + s[-22:-17] + s[:6]
-print(s)
+str = str[39:67] + "with " + str[:6]
+print(str)
