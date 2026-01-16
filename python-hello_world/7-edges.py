b465dd8d1fb54d5e08fb3e320a48cbdcd460d018
dufontdd <dufont.dd@gmail.com>
2026-01-16 12:40:53 +0100

    feat print a specific character of a strings

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,7 +3,7 @@ word = "Holberton"
 word_first_3 = "Hol"
 word_last_2 = "on"
 middle_word = "olberto"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
+# end
