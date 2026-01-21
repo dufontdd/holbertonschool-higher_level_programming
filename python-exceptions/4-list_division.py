141376dd54b86465a2238ad1073a0ffd8cc9f22f
dufontdd <dufont.dd@gmail.com>
2026-01-21 12:49:48 +0100

    feat: Write a function that divides element by element 2 lists

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+
+    for i in range(list_length):
+        try:
+            div = my_list_1[i] / my_list_2[i]
+        except (TypeError, ZeroDivisionError, IndexError):
+            div = 0
+        finally:
+            result.append(div)
+    
+    return result
