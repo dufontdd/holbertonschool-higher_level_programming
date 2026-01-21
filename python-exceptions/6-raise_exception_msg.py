c45e185aaa8820bee5d7a1570e878aecf583fc84
dufontdd <dufont.dd@gmail.com>
2026-01-21 17:20:28 +0100

    feat: Write a function that raises a name exception with a message

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import sys
-
-def safe_print_integer_err(value):
-    try:
-        print("{:d}".format(value))
-        return len(str(value))
-    except (TypeError, ValueError) as e:
-        sys.stderr.write("Exception: {}\n".format(e))
-        return False
+def raise_exception_msg(message=""):
+    raise NameError(message)
