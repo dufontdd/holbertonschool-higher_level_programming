fa3191016762424a4cf1f35538741094d082c9fd
dufontdd <dufont.dd@gmail.com>
2026-01-21 16:47:11 +0100

    feat: Write a function that computes the square value of all integers of a matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # Utilise list comprehension pour créer une nouvelle matrice
-    return [[x ** 2 for x in row] for row in matrix
+    return [[x ** 2 for x in row] for row in matrix]
