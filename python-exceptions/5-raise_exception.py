9e947bf4b4b373ef16e999f06b7f7311afb27ef1
dufontdd <dufont.dd@gmail.com>
2026-01-21 17:11:30 +0100

    feat: Write a function that raises a type exception

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except (TypeError, ValueError):
-        return False
+def raise_exception():
+    raise TypeError
