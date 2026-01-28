8989665ab5b97b4b6caf71cfab5462802f8f0a01
dufontdd <dufont.dd@gmail.com>
2026-01-28 15:28:48 +0100

    feat Write an empty class Square that defines a square whithout import any module

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-"""
-This is an empty class Square that defines a square.
-"""
+Square = __import__('0-square').Square
 
-
-class Square:
-    """
-    An empty class that defines a square.
-    """
-    pass)
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
