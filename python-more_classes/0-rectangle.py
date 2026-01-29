724becedb868a3b6b44c9cb6d9efaf0683fd8df3
dufontdd <dufont.dd@gmail.com>
2026-01-29 22:55:02 +0100

    feat: Write an empty class Rectangle that defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is an empty class Rectangle that defines a rectangle.
+"""
+
+
+class Rectangle:
+    """
+    An empty class that defines a rectangle.
+    """
+    pass
