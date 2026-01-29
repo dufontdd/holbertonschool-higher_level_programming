9c2520aa9cd2fb9354fff9326fccea0b75dbb4eb
dufontdd <dufont.dd@gmail.com>
2026-01-29 23:01:55 +0100

    feat: Write a class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -58,3 +58,12 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle = []
+        for _ in range(self.__height):
+            rectangle.append("#" * self.__width)
+        return "\n".join(rectangle)
