da20df75a1773fffae85b01d83cdebfe6bc677c6
dufontdd <dufont.dd@gmail.com>
2026-02-11 15:00:49 +0100

    feat: Write a script that adds all arguments to a Python list, and then save them to a file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that adds command line arguments to a list
+and saves them in a JSON file.
+"""
+
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    my_list = load_from_json_file(filename)
+except Exception:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)
