432625757339c78cb88c405064a026d71946f509
dufontdd <dufont.dd@gmail.com>
2026-02-11 15:50:23 +0100

    feat: Write a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""
-Function that returns a dictionary description of an object
-for JSON serialization.
-"""
-
 def class_to_json(obj):
     """
-    Args:
-        obj: an instance of a class
-    Returns:
-        dictionary of all serializable attributes of obj
+    Returns a dictionary of all serializable attributes
+    (instance and class attributes) of obj.
     """
-    return obj.__dict__
+    result = {}
+    for key in dir(obj):
+        if key.startswith('__'):
+            continue
+        value = getattr(obj, key)
+        if isinstance(value, (int, float, str, bool, list, dict)):
+            result[key] = value
+    return result
