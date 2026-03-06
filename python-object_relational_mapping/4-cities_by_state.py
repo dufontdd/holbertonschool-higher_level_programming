c9094d1bb06e09ec6b542cb5a66377460b519626
dufontdd <dufont.dd@gmail.com>
2026-03-06 15:54:27 +0100

    feat: a script that lists all cities from the database hbtn_0e_4_usa

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,33 +1,40 @@
 #!/usr/bin/python3
-"""List all cities with their state"""
-
-import MySQLdb
+"""
+4-cities_by_state.py
+Script that lists all cities from the database hbtn_0e_4_usa
+"""
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
+    """
+    Main function to connect to the database,
+    And retrieve all the cities.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
     )
 
-    cur = db.cursor()
+    cursor = db.cursor()
 
-    query = """
-    SELECT cities.id, cities.name, states.name
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    ORDER BY cities.id ASC
-    """
+    query = (
+        "SELECT cities.id, cities.name, states.name FROM cities "
+        "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC"
+    )
+    cursor.execute(query)
+    cities = cursor.fetchall()
 
-    cur.execute(query)
+    for city in cities:
+        print(city)
 
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cur.close()
+    cursor.close()
     db.close()
