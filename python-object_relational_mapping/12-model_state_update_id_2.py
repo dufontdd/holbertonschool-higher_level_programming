c3c88a24887d28ac5d854fbb14360c81f1a80adc
dufontdd <dufont.dd@gmail.com>
2026-03-06 16:07:56 +0100

    feat: a script that changes the name of a State object from the database hbtn_0e_6_usa

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Update State with id = 2"""
-
+"""
+12-model_state_update_id_2.py
+Script that changes the name of a State object in the database hbtn_0e_6_usa.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-
 if __name__ == "__main__":
+    """Main function."""
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True)
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True
+    )
 
     Session = sessionmaker(bind=engine)
     session = Session()
