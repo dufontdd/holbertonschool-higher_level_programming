ff35f1d2fa4d844d716f8a78b28f47ddfacde7cf
dufontdd <dufont.dd@gmail.com>
2026-03-06 15:55:26 +0100

    feat: a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python3
-"""Lists all cities of a state"""
-
-import MySQLdb
+"""
+5-filter_cities.py
+Script that takes in the name of a state as an argument,
+And lists all cities of that state.
+"""
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
+    """
+    Main function to connect to the database,
+    And retrieve all cities from given state.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
     )
 
-    cur = db.cursor()
+    cursor = db.cursor()
 
-    query = """
-    SELECT GROUP_CONCAT(cities.name ORDER BY cities.id SEPARATOR ', ')
-    FROM cities
-    JOIN states ON cities.state_id = states.id
-    WHERE states.name = %s
-    """
+    query = (
+        "SELECT cities.name FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
+    cursor.execute(query, (state_name,))
+    cities = cursor.fetchall()
 
-    cur.execute(query, (sys.argv[4],))
+    print(", ".join(city[0] for city in cities))
 
-    result = cur.fetchone()
-
-    if result[0] is not None:
-        print(result[0])
-
-    cur.close()
+    cursor.close()
     db.close()
