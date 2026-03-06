1b58e93bb27dcfdca2387a0d406d36d50ab4bf56
dufontdd <dufont.dd@gmail.com>
2026-03-06 15:53:12 +0100

    feat: Wait, do you remember the previous task? Did you test Arizona; TRUNCATE TABLE states ; SELECT * FROM states WHERE name =  as an input?

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,40 @@
 #!/usr/bin/python3
-"""Safe filter states by user input"""
+"""
+3-my_safe_filter_states.py
+Displays all values in the states table of hbtn_0e_0_usa,
+Where name matches the argument.
+"""
 
-import MySQLdb
 import sys
+import MySQLdb
+
 
 if __name__ == "__main__":
+    """
+    Main function to connect to the database,
+    And retrieve matching states.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
     db = MySQLdb.connect(
         host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database,
+        port=3306
     )
 
-    cur = db.cursor()
+    cursor = db.cursor()
 
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (sys.argv[4],))
+    cursor.execute(query, (state_name,))
+    states = cursor.fetchall()
 
-    rows = cur.fetchall()
+    for state in states:
+        print(state)
 
-    for row in rows:
-        print(row)
-
-    cur.close()
+    cursor.close()
     db.close()
